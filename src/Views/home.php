4ed4fc16f8f617e7470550860e6c71657980431b
jacques <jac.granarolo.info@gmail.com>
2023-09-09 12:39:59 +0200

    css 1

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,7 +1,8 @@
-<main class="">
+<main class="d-flex flex-column align-items-center">
     <?php
     echo '<h2>' . $titlePage . '</h2>';
     if ($isConnected) {
+        /*
         if (count($projetsDirected) > 0) {
             echo '<h3>Projet dirigés :</h3>';
             foreach ($projetsDirected as $projet) {
@@ -37,11 +38,56 @@
                     . '">&#128196; Voir</a>'
                     . '<br />';
             }
+        }*/
+
+        if (count($projetsDirected) > 0) {
+            echo '<h3>Projet dirigés :</h3>';
+            echo '<table>';
+            echo '<tr><th>ID</th><th>Nom</th><th>Description</th><th>ID Directeur</th><th>Actions</th></tr>';
+            foreach ($projetsDirected as $projet) {
+                echo '<tr>'
+                    . '<td>' . $projet->getId_projet() . '</td>'
+                    . '<td>' . $projet->getTitre() . '</td>'
+                    . '<td>' . $projet->getDescription() . '</td>'
+                    . '<td>' . $projet->getId_utilisateur() . '</td>'
+                    . '<td>'
+                    . '<a href="index.php?page=Projet&method=view&id=' . $projet->getId_projet() . '" class="btn-01-sm bg-color02-03">&#128196; Voir</a>'
+                   
+                    . '<a href="index.php?page=Projet&method=edit&id=' . $projet->getId_projet() . '" class="btn-01-sm bg-color02-03">&#9998; Editer</a>'
+                    
+                    . '<a href="index.php?page=Home&method=deleteProjet&id=' . $projet->getId_projet() . '" class="btn-01-sm bg-color02-03">X Supprimer</a>'
+                    . '</td>'
+                    . '</tr>';
+            }
+            echo '</table>';
         }
+        
+        if (count($projetsParticip) > 0) {
+            echo '<h3>Projet avec participation :</h3>';
+            echo '<table>';
+            echo '<tr><th>ID</th><th>Nom</th><th>Description</th><th>ID Directeur</th><th>Actions</th></tr>';
+            foreach ($projetsParticip as $projet) {
+                echo '<tr>'
+                    . '<td>' . $projet->getId_projet() . '</td>'
+                    . '<td>' . $projet->getTitre() . '</td>'
+                    . '<td>' . $projet->getDescription() . '</td>'
+                    . '<td>' . $projet->getId_utilisateur() . '</td>'
+                    . '<td>'
+                    . '<a href="index.php?page=Projet&method=view&id=' . $projet->getId_projet() . '" class="btn-01-sm bg-color02-03">&#128196; Voir</a>'
+                    . '</td>'
+                    . '</tr>';
+            }
+            echo '</table>';
+        }
+        
+
         echo '<br />';
-        echo '<a href="index.php?page=Projet&method=create">Créer un projet</a>';
+        echo '<a href="index.php?page=Projet&method=create" class="btn-01 bg-color01-01">Créer un projet</a>';
     } else {
         echo '<h3>Veuillez vous connecter</h3>';
     }
     ?>
+    <div id="bloc-grow">
+        
+    </div>
 </main>
